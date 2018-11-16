26b3fcd765f5477bb0074e0303f77d96e165fe0d
161099 <audedpt@gmail.com>
2018-11-16 16:41:52 +0100

    exo 1 et 2

--- src/main/java/exercise1/ExceptionCatcher.java
+++ src/main/java/exercise1/ExceptionCatcher.java
@@ -8,6 +8,11 @@ public class ExceptionCatcher {
 	}
 
 	void executeExceptionThrowerSafely(int value) {
-		exceptionThrower.throwACustomExceptionWhenValueIs42(value);
-	}
+		try {
+			exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+		}
+		catch (RuntimeException e) {
+
+		}
+		}
 }
--- src/main/java/exercise1/ExceptionThrower.java
+++ src/main/java/exercise1/ExceptionThrower.java
@@ -1,7 +1,13 @@
 package exercise1;
+import	java.lang.RuntimeException;
+
 
 public class ExceptionThrower {
-	void throwACustomExceptionWhenValueIs42(int value) {
-
+	void throwACustomExceptionWhenValueIs42(int value)
+	throws MyException
+	{
+		if (value == 42){
+			throw new MyException();
+		}
 	}
 }
